9906a429a5e5dced39819e2d25d0ca9aa83a559c
nickey9891 <nickey.magee@gmail.com>
2026-09-01 01:03:33 -0400

    Expand seasonality universe to 75 stocks

---
+++ tickers-75.js
@@ -0,0 +1,3 @@
+'use strict';
+// Expand the base 50-stock universe to 75 established, liquid U.S. stocks.
+TICKERS.push('AXP','CVX','OXY','SLB','IBM','CSCO','AMAT','KLAC','LRCX','AMGN','GILD','BMY','JNJ','TMO','DHR','UPS','FDX','MMM','HON','GM','F','BKNG','CMCSA','T','VZ');
